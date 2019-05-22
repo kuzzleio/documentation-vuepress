48dcf651d913fb6278280b9066c3b4ce08a9e49f
Luca Marchesini <xbill82@gmail.com>
2019-05-22 17:23:34 +0200

    [KZL-1036] Add available plugins

--- src/.vuepress/components/SDKIndex.vue
+++ src/.vuepress/components/SDKIndex.vue
@@ -1,73 +1,73 @@
 <template>
-  <div class="Languages">
-    <a :href="generateLink('/sdk/js/6/')" class="Languages-item">
-      <img src="/logos/javascript.svg" alt="js logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Javascript</div>
+  <div class="Tiles">
+    <a :href="generateLink('/sdk/js/6/')" class="Tiles-item">
+      <img src="/logos/javascript.svg" alt="js logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Javascript</div>
     </a>
-    <a :href="generateLink('/sdk/go/1/')" class="Languages-item">
+    <a :href="generateLink('/sdk/go/1/')" class="Tiles-item">
       <div class="ribbon">
         <span>BETA</span>
       </div>
-      <img src="/logos/go.svg" alt="golang logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Golang</div>
+      <img src="/logos/go.svg" alt="golang logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Golang</div>
     </a>
-    <a :href="generateLink('/sdk/cpp/1/')" class="Languages-item">
+    <a :href="generateLink('/sdk/cpp/1/')" class="Tiles-item">
       <div class="ribbon">
         <span>BETA</span>
       </div>
-      <img src="/logos/cpp.svg" alt="c++ logo" class="Languages-item-logo">
-      <div class="Languages-item-name">C++</div>
+      <img src="/logos/cpp.svg" alt="c++ logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">C++</div>
     </a>
-    <a :href="generateLink('/sdk/java/1/')" class="Languages-item">
+    <a :href="generateLink('/sdk/java/1/')" class="Tiles-item">
       <div class="ribbon">
         <span>BETA</span>
       </div>
-      <img src="/logos/java.svg" alt="java logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Java</div>
+      <img src="/logos/java.svg" alt="java logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Java</div>
     </a>
 
-    <a :href="generateLink('/sdk/php/3/')" class="Languages-item">
-      <img src="/logos/php.svg" alt="php logo" class="Languages-item-logo">
-      <div class="Languages-item-name">PHP</div>
+    <a :href="generateLink('/sdk/php/3/')" class="Tiles-item">
+      <img src="/logos/php.svg" alt="php logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">PHP</div>
     </a>
-    <a :href="generateLink('/sdk/android/3/')" class="Languages-item">
-      <img src="/logos/android.svg" alt="android logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Android</div>
+    <a :href="generateLink('/sdk/android/3/')" class="Tiles-item">
+      <img src="/logos/android.svg" alt="android logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Android</div>
     </a>
-    <a href="https://github.com/kuzzleio/sdk-ruby" class="Languages-item">
+    <a href="https://github.com/kuzzleio/sdk-ruby" class="Tiles-item">
       <div class="ribbon">
         <span>ALPHA</span>
       </div>
-      <img src="/logos/ruby.svg" alt="ruby logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Ruby</div>
+      <img src="/logos/ruby.svg" alt="ruby logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Ruby</div>
     </a>
-    <a href="https://github.com/kuzzleio/sdk-python2" class="Languages-item">
+    <a href="https://github.com/kuzzleio/sdk-python2" class="Tiles-item">
       <div class="ribbon">
         <span>ALPHA</span>
       </div>
-      <img src="/logos/python.svg" alt="python logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Python 2</div>
+      <img src="/logos/python.svg" alt="python logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Python 2</div>
     </a>
-    <a href="https://github.com/kuzzleio/sdk-python3" class="Languages-item">
+    <a href="https://github.com/kuzzleio/sdk-python3" class="Tiles-item">
       <div class="ribbon">
         <span>ALPHA</span>
       </div>
-      <img src="/logos/python.svg" alt="python logo" class="Languages-item-logo">
-      <div class="Languages-item-name">Python 3</div>
+      <img src="/logos/python.svg" alt="python logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Python 3</div>
     </a>
-    <a href="https://github.com/kuzzleio/sdk-lua" class="Languages-item">
+    <a href="https://github.com/kuzzleio/sdk-lua" class="Tiles-item">
       <div class="ribbon">
         <span>ALPHA</span>
       </div>
-      <img src="/logos/lua.svg" alt="lua logo" class="Languages-item-logo">
-      <div class="Languages-item-name">LUA</div>
+      <img src="/logos/lua.svg" alt="lua logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">LUA</div>
     </a>
-    <a href="https://github.com/kuzzleio/sdk-csharp" class="Languages-item">
+    <a href="https://github.com/kuzzleio/sdk-csharp" class="Tiles-item">
       <div class="ribbon">
         <span>ALPHA</span>
       </div>
-      <img src="/logos/csharp.svg" alt="csharp logo" class="Languages-item-logo">
-      <div class="Languages-item-name">C#</div>
+      <img src="/logos/csharp.svg" alt="csharp logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">C#</div>
     </a>
   </div>
 </template>
